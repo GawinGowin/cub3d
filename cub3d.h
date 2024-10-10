0cf0f17771870ef1bfe34aaafd174dab14bca63c
syamasaw <138424335+Sawawa42@users.noreply.github.com>
2024-10-10 20:56:27 +0900

    8 .cubファイルの読み込み (#9)
    * .cubのテストファイルとgitignoreにcub3Dを追加
    
    * file->string
    
    * file->char**

--- cub3d.h
+++ cub3d.h
@@ -16,12 +16,26 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdint.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 600
+# endif
 
 int		printerror(char *str);
 int		valid_argument(int argc, char **argv);
+char	**set_array_from_file(char *filename);
 
 // libftの関数
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memset(void *ptr, int value, size_t num);
+char	**ft_split(char const *s, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *restrict dest, const char *restrict src, size_t size);
 
 // libft外の関数
 int		ft_strcmp(const char *s1, const char *s2);
--- main.c
+++ main.c
@@ -14,9 +14,22 @@
 
 int	main(int argc, char **argv)
 {
+	char	**cubfile_array;
+	int		i;
+
 	if (valid_argument(argc, argv))
 		return (1);
-	// ここに処理
+	cubfile_array = set_array_from_file(argv[1]);
+	if (!cubfile_array)
+		return (1);
+	// 確認用
+	while (cubfile_array[i])
+	{
+		printf("> %s\n", cubfile_array[i]);
+		free(cubfile_array[i]);
+		i++;
+	}
+	free(cubfile_array);
 	return (0);
 }
 
