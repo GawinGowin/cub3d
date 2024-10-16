b6686875f461c45699506eb6f2aad013a0a9c7ac
GawinGowin <>
2024-10-16 07:33:20 +0000

    fix: テストケースの追加と修正
    CMakeLists.txtの修正により、テストケースの追加と修正を行いました。

--- tests/test_map_validation.cpp
+++ tests/test_map_validation.cpp
@@ -1,127 +1,89 @@
 #include <gtest/gtest.h>
 
-extern "C" {
-    #include "cub3d.h"
+extern "C"
+{
+#include "cub3d.h"
 }
 
-TEST(MapValidation, MapOK1) {
-	const char *map[] = {
-		"11111111",
-		"10000001",
-		"10000001",
-		"10000001",
-		"10000001",
-		"10000001",
-		"11111111",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 1);
+struct MapTestData
+{
+	const char *map[10];
+	int expected; // 期待される結果
+};
+
+// テストデータの作成
+class MapValidationTest : public ::testing::TestWithParam<MapTestData>
+{
+};
+
+TEST_P(MapValidationTest, ValidateMap)
+{
+	auto param = GetParam();
+	EXPECT_EQ(is_valid_map((char **)param.map), param.expected);
 }
 
-TEST(MapValidation, MapOK2) {
-	const char *map[] = {
-		"11111111",
-		"10000001",
-		"10000001",
-		"10000001",
-		"10000111",
-		"100001  ",
-		"111111  ",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 1);
-}
+// 有効なマップ
+INSTANTIATE_TEST_SUITE_P(
+	ValidMaps,
+	MapValidationTest,
+	::testing::Values(
+		MapTestData{{"11111111",
+					 "10000001",
+					 "10000001",
+					 "10000001",
+					 "10000001",
+					 "10000001",
+					 "11111111", NULL},
+					1},
+		MapTestData{{"11111111",
+					 "10000001",
+					 "10000001",
+					 "10000111",
+					 "100001  ",
+					 "111111  ", NULL},
+					1},
+		MapTestData{{"  111111",
+					 "  100001",
+					 "11100001",
+					 "10000001",
+					 "10000111",
+					 "100001  ",
+					 "111111  ", NULL},
+					1}
+	)
+);
 
-TEST(MapValidation, MapOK3) {
-	const char *map[] = {
-		"  111111",
-		"  100001",
-		"11100001",
-		"10000001",
-		"10000111",
-		"100001  ",
-		"111111  ",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 1);
-}
-
-TEST(MapValidation, MapOK4) {
-	const char *map[] = {
-		"  111111",
-		"  100111",
-		"11100001",
-		"10000001",
-		"10000111",
-		"111001  ",
-		"111111  ",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 1);
-}
-
-TEST(MapValidation, MapOK5) {
-	const char *map[] = {
-		"        ",
-		" 111111 ",
-		" 100001 ",
-		" 100001 ",
-		" 100001 ",
-		" 111111 ",
-		"        ",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 1);
-}
-
-
-TEST(MapValidation, MapERR1) {
-	const char *map[] = {
-		"11111111",
-		"10000001",
-		"10000001",
-		"1000000 ",
-		"10000001",
-		"10000001",
-		"11111111",
-		NULL
-	};
-	int ret = is_valid_map((char **)map);
-	EXPECT_EQ(ret, 0);
-}
-
-TEST(MapValidation, MapERR2) {
-	const char *map[] = {
-		"11111111",
-		"10000001",
-		"10000000",
-		"10000000",
-		"10000001",
-		"10000001",
-		"11111111",
-		NULL
-	};
-	int ret = is_valid_map((char **)map);
-	EXPECT_EQ(ret, 0);
-}
-
-
-TEST(MapValidation, MapERR3) {
-	const char *map[] = {
-		"        ",
-		"        ",
-		"        ",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 0);
-}
-
-TEST(MapValidation, MapERR4) {
-	const char *map[] = {
-		"1111111111",
-		"1111111111",
-		"1111111111",
-		NULL
-	};
-	EXPECT_EQ(is_valid_map((char **)map), 0);
-}
+INSTANTIATE_TEST_SUITE_P(
+	InvalidMaps,
+	MapValidationTest,
+	::testing::Values(
+		MapTestData{{"11111111",
+					 "10000001",
+					 "10000001",
+					 "1000000 ",
+					 "10000001",
+					 "11111111", NULL},
+					0},
+		MapTestData{{"        ",
+					 "        ",
+					 "        ",
+					 NULL},
+					0},
+		MapTestData{{"1111111111",
+					 "1111111111",
+					 "1111111111",
+					 NULL},
+					0},
+		MapTestData{{"1111111111",
+					 NULL},
+					0},
+		MapTestData{{"1",
+					 "1",
+					 "1",
+					 "1",
+					 "1",
+					 "1",
+					 NULL},
+					0}
+	)
+);
