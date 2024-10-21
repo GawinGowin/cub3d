df0a9f16862b4c11bdee05c519dfadb4a9dddbc7
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-10-21 20:35:57 +0900

    update(validation): プレイヤー位置のvalidationを追加 (#52)

--- cmd/cub3D/validation/validation.c
+++ cmd/cub3D/validation/validation.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 17:04:08 by saraki            #+#    #+#             */
-/*   Updated: 2024/10/20 19:50:12 by saraki           ###   ########.fr       */
+/*   Updated: 2024/10/21 11:29:14 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ static int	is_data_satisfies(t_data *data)
 {
 	if (data == NULL || data->params.map == NULL)
 		return (0);
-	if (!data->params.img_no)
+	if (!data->params.img_no || !data->params.img_so
+		|| !data->params.img_ea || !data->params.img_we)
 		return (0);
-	if (!data->params.img_so)
-		return (0);
-	if (!data->params.img_ea)
-		return (0);
-	if (!data->params.img_we)
-		return (0);
-	if (data->params.ceiling < 0)
-		return (0);
-	if (data->params.floor < 0)
+	if (data->params.ceiling < 0 || data->params.floor < 0)
 		return (0);
 	if (data->params.map_width == 0 || data->params.map_height == 0)
+		return (0);
+	if (!(data->player.pos_x > 0) || !(data->player.pos_y > 0)
+		|| !(data->player.pos_x < data->params.map_width - 1)
+		|| !(data->player.pos_y < data->params.map_height - 1))
+		return (0);
+	if (!(data->player.angle == 0) || !(data->player.angle == 90)
+		|| !(data->player.angle == 180) || !(data->player.angle == 270))
 		return (0);
 	return (1);
 }
