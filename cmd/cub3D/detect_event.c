d3ea17cc6282f2ef716138eb53662e1a8e2078a7
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-10-28 05:05:09 +0900

    revert(#58) (#60)
    *  #58をrevertする

--- cmd/cub3D/detect_event.c
+++ cmd/cub3D/detect_event.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 06:17:26 by syamasaw          #+#    #+#             */
-/*   Updated: 2024/10/27 09:37:57 by saraki           ###   ########.fr       */
+/*   Updated: 2024/10/27 19:49:19 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,27 @@ int	detect_close(t_data *data)
 	destroy_mlx_ptr(data);
 	exit (0);
 	return (0);
+}
+
+// mlx_get_data_addrはimg_ptr->dataを返すため、mlx_destroy_imageで解放される。
+int	destroy_mlx_ptr(t_data *data)
+{
+	if (data->mlx_val.mlx_ptr && data->params.img_no)
+		mlx_destroy_image(data->mlx_val.mlx_ptr, data->params.img_no);
+	if (data->mlx_val.mlx_ptr && data->params.img_so)
+		mlx_destroy_image(data->mlx_val.mlx_ptr, data->params.img_so);
+	if (data->mlx_val.mlx_ptr && data->params.img_we)
+		mlx_destroy_image(data->mlx_val.mlx_ptr, data->params.img_we);
+	if (data->mlx_val.mlx_ptr && data->params.img_ea)
+		mlx_destroy_image(data->mlx_val.mlx_ptr, data->params.img_ea);
+	if (data->mlx_val.mlx_ptr && data->mlx_val.img_ptr)
+		mlx_destroy_image(data->mlx_val.mlx_ptr, data->mlx_val.img_ptr);
+	if (data->mlx_val.mlx_ptr && data->mlx_val.win_ptr)
+		mlx_destroy_window(data->mlx_val.mlx_ptr, data->mlx_val.win_ptr);
+	if (data->mlx_val.mlx_ptr)
+	{
+		mlx_destroy_display(data->mlx_val.mlx_ptr);
+		free(data->mlx_val.mlx_ptr);
+	}
+	return (1);
 }
