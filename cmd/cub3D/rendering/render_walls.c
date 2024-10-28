dae2fd2cb66e49f55921c5266aaf885adaa1a55b
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-10-28 18:00:38 +0000

    fix(render): 魚眼を補正

--- cmd/cub3D/rendering/render_walls.c
+++ cmd/cub3D/rendering/render_walls.c
@@ -6,13 +6,13 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 06:27:49 by saraki            #+#    #+#             */
-/*   Updated: 2024/10/28 17:09:43 by saraki           ###   ########.fr       */
+/*   Updated: 2024/10/28 17:59:39 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	rotate_player(t_player *player, double x);
+static double	rotate_player(t_player *adjusted_player, double x);
 static void	render_a_line(t_mlx_val *mlx,
 				t_param_cub *params, size_t x, t_dda *dda);
 
@@ -21,13 +21,15 @@ void	render_walls(t_mlx_val *mlx, t_player *player, t_param_cub *params)
 	size_t		x;
 	t_player	adjusted_player;
 	t_dda		dda_ret;
+	double		elevation_angle;
 
 	x = 0;
 	while (x < WIN_WIDTH)
 	{
 		adjusted_player = *player;
-		rotate_player(&adjusted_player, (double) x);
+		elevation_angle = rotate_player(&adjusted_player, (double) x);
 		dda_ret = dda(&adjusted_player, params->map);
+		dda_ret.distance *= elevation_angle;
 		render_a_line(mlx, params, x, &dda_ret);
 		x ++;
 	}
@@ -62,7 +64,7 @@ static void	render_a_line(t_mlx_val *mlx,
 	}
 }
 
-static void	rotate_player(t_player *adjusted_player, double x)
+static double	rotate_player(t_player *adjusted_player, double x)
 {
 	double		dir[2];
 	double		plane[2];
@@ -71,16 +73,15 @@ static void	rotate_player(t_player *adjusted_player, double x)
 	double		camera_x;
 
 	camera_x = 2 * x / (double)WIN_WIDTH - 1;
-	angle = (double) adjusted_player->angle;
 	plane_org[0] = 0;
 	plane_org[1] = tan(FOV / 2.0 * (PI / 180.0));
-	dir[0] = cos_degree(angle);
-	dir[1] = sin_degree(angle);
+	dir[0] = cos_degree(adjusted_player->angle);
+	dir[1] = sin_degree(adjusted_player->angle);
 	plane[0] = plane_org[0] * dir[0] - plane_org[1] * dir[1];
 	plane[1] = plane_org[0] * dir[1] + plane_org[1] * dir[0];
 	dir[0] = dir[0] + plane[0] * camera_x;
 	dir[1] = dir[1] + plane[1] * camera_x;
-	angle = atan2(dir[1], dir[0]) * (180.0 / PI);
-	adjusted_player->angle = regulate_angle(angle);
-	return ;
+	angle = adjusted_player->angle;
+	adjusted_player->angle = atan2(dir[1], dir[0]) * (180.0 / PI);
+	return (cos_degree(angle - adjusted_player->angle));
 }
