7dc250fa1617684105c489262db30f4fd7388239
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-11-04 02:26:38 +0900

    73-壁に接近時の描画を修正 (#77)
    * fix(texture): 描画されるテクスチャの縦方向の成分が間違っていた
    
    * chore(render): エラーハンドリングを追加
    
    * norminette

--- cmd/cub3D/update_screen.c
+++ cmd/cub3D/update_screen.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 04:14:23 by saraki            #+#    #+#             */
-/*   Updated: 2024/11/03 00:43:00 by saraki           ###   ########.fr       */
+/*   Updated: 2024/11/03 17:25:04 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 void	update_screen(t_data *data)
 {
-	/* renderring calcuration start */
 	render_bg(&(data->mlx_val), data->params.ceiling, data->params.floor);
-	render_walls(data, &(data->player)); // TODO: error handling
-
-	/* renderring calcuration end */
-	if (render_image(&(data->mlx_val)))
+	if (render_walls(data, &(data->player)))
+	{
+		printerror(ERR_FAILED_RENDERING);
 		deinit_exit(data, 1);
+	}
+	if (render_image(&(data->mlx_val)))
+	{
+		printerror(ERR_FAILED_RENDERING);
+		deinit_exit(data, 1);
+	}
 	return ;
 }
