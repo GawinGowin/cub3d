c9ac2c6f7ed6ff06aeeaa838ee1cea70f22f3a07
GawinGowin <>
2024-11-05 11:14:12 +0000

    rm: 提出前

--- tests/parse_cub/compare_operators.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef COMPARE_OPERATORS_HPP
-#define COMPARE_OPERATORS_HPP
-
-#include "cub3d.h"
-bool operator==(const t_param_cub &a, const t_param_cub &b);
-bool operator==(const t_player &a, const t_player &b);
-bool operator!=(const t_texture &a, const t_texture &b);
-
-#endif // COMPARE_OPERATORS_HPP
